5cdb12faab2bcc593f0923f291152f81ed0d87d1
Kelvin Kellner <kelvinkellner123@gmail.com>
2022-12-01 13:23:21 -0500

    Update improve for improved clarity of states and added realism

--- traffic.py
+++ traffic.py
@@ -19,29 +19,60 @@ sensors_active = [False, False]
 window = tk.Tk()
 
 frames = []
+frames_label = []
 for i in range(ROWS):
     temp = []
+    temp2 = []
     for j in range(COLS):
-        frame = tk.Frame(
+        frame1 = tk.Frame(
             master=window,
-            relief=tk.GROOVE,
-            borderwidth=12,
             width=94,
             height=94,
-            bg=color_mapping[state][j][i]
+            bg='gray'#color_mapping[state][j][ROWS-i-1] if j % 2 == 0 else color_mapping[state][j][i]
         )
-        frame.grid(row=i, column=j, padx=5, pady=0)
-        temp.append(frame)
+        frame2 = tk.Frame(
+            master=window,
+            width=94,
+            height=94,
+            bg='gray'#color_mapping[state][j][i] if j % 2 == 0 else color_mapping[state][j][ROWS-i-1]
+        )
+        if j%2 == 0:
+            label1 = tk.Label(master=frame1, text="A", width=8, height=4,
+            relief=tk.GROOVE,
+            borderwidth=12, bg=color_mapping[state][j][ROWS-i-1] if j % 2 == 0 else color_mapping[state][j][i])
+            label2 = tk.Label(master=frame2, text="A", width=8, height=4,
+            relief=tk.GROOVE,
+            borderwidth=12, bg=color_mapping[state][j][i] if j % 2 == 0 else color_mapping[state][j][ROWS-i-1])
+            label1.pack()
+            label2.pack()
+            frame1.grid(row=1+i, column=j, padx=5, pady=5)
+            frame2.grid(row=1+i, column=4+j, padx=5, pady=5)
+        else:
+            label1 = tk.Label(master=frame1, text="B", width=8, height=4,
+            relief=tk.GROOVE,
+            borderwidth=12, bg=color_mapping[state][j][ROWS-i-1] if j % 2 == 0 else color_mapping[state][j][i])
+            label2 = tk.Label(master=frame2, text="B", width=8, height=4,
+            relief=tk.GROOVE,
+            borderwidth=12, bg=color_mapping[state][j][i] if j % 2 == 0 else color_mapping[state][j][ROWS-i-1])
+            label1.pack()
+            label2.pack()
+            frame1.grid(row=0, column=i+j, padx=5, pady=5)
+            frame2.grid(row=4, column=i+j, padx=5, pady=5)
+        temp.append(frame1)
+        temp.append(frame2)
+        temp2.append(label1)
+        temp2.append(label2)
     frames.append(temp)
+    frames_label.append(temp2)
 panel = tk.Frame(master=window, width=100, height=100)
-label = tk.Label(master=panel, text="Press 'a' or 'b' key!")
+label = tk.Label(master=panel, text="Press the 'a' or 'b' key to activate traffic sensors!")
 label.pack()
-label_state = tk.Label(master=panel, text="State: {}".format(state))
+label_state = tk.Label(master=panel, text="State: S{}".format(state))
 label_state.pack()
-label_sensors_active = tk.Label(master=panel, text="Sensors active: {}, {}".format(str(sensors_active[0])[:1], str(sensors_active[1])[:1]))
+label_sensors_active = tk.Label(master=panel, text="Sensors active...\nA: {:<5s}\nB: {:<5s}".format(str(sensors_active[0]).upper(), str(sensors_active[1]).upper()))
 label_sensors_active.pack()
-# panel.place(x=205, y=0)
-panel.grid(row=0, column=2, rowspan=3, padx=5, pady=0)
+print(frames)
+panel.grid(row=2, column=1, columnspan=3, padx=0, pady=0)
         
 def handle_keypress(event):
     """Print the character associated to the key pressed"""
@@ -50,7 +81,7 @@ def handle_keypress(event):
         sensors_active[0] = True
     elif event.char == 'b':
         sensors_active[1] = True
-    label_sensors_active.configure(text="Sensors active: {}, {}".format(str(sensors_active[0])[:1], str(sensors_active[1])[:1]))
+    label_sensors_active.configure(text="Sensors active...\nA: {:<5s}\nB: {:<5s}".format(str(sensors_active[0]).upper(), str(sensors_active[1]).upper()))
     print(event.char)
 
 def next_state():
@@ -59,23 +90,26 @@ def next_state():
         if state == 1:
             if sensors_active[0]:
                 sensors_active[0] = False
-                label_sensors_active.configure(text="Sensors active: {}, {}".format(str(sensors_active[0])[:1], str(sensors_active[1])[:1]))
+                label_sensors_active.configure(text="Sensors active...\nA: {:<5s}\nB: {:<5s}".format(str(sensors_active[0]).upper(), str(sensors_active[1]).upper()))
                 if not sensors_active[1]:
                     window.after(time_cycle[state], next_state)
                     return
         if state == 4:
             if sensors_active[1]:
                 sensors_active[1] = False
-                label_sensors_active.configure(text="Sensors active: {}, {}".format(str(sensors_active[0])[:1], str(sensors_active[1])[:1]))
+                label_sensors_active.configure(text="Sensors active...\nA: {:<5s}\nB: {:<5s}".format(str(sensors_active[0]).upper(), str(sensors_active[1]).upper()))
                 if not sensors_active[0]:
                     window.after(time_cycle[state], next_state)
                     return
     state = (state + 1) % len(color_mapping)
     for i in range(ROWS):
         for j in range(COLS):
-            frames[i][j].configure(bg=color_mapping[state][j][i])
-    label_state.configure(text="State: {}".format(state))
-    label_sensors_active.configure(text="Sensors active: {}, {}".format(str(sensors_active[0])[:1], str(sensors_active[1])[:1]))
+            # frames[i][(j*2)+0].configure(bg=color_mapping[state][j][ROWS-i-1] if j % 2 == 0 else color_mapping[state][j][i])
+            # frames[i][(j*2)+1].configure(bg=color_mapping[state][j][i] if j % 2 == 0 else color_mapping[state][j][ROWS-i-1])
+            frames_label[i][(j*2)+0].configure(bg=color_mapping[state][j][ROWS-i-1] if j % 2 == 0 else color_mapping[state][j][i])
+            frames_label[i][(j*2)+1].configure(bg=color_mapping[state][j][i] if j % 2 == 0 else color_mapping[state][j][ROWS-i-1])
+    label_state.configure(text="State: S{}".format(state))
+    label_sensors_active.configure(text="Sensors active...\nA: {:<5s}\nB: {:<5s}".format(str(sensors_active[0]).upper(), str(sensors_active[1]).upper()))
     window.update()
     window.after(time_cycle[state], next_state)
 
